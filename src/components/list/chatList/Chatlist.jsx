b33833e1e46518752e951423a8a746c177dde566
Guocork <g1024536444@gmail.com>
2024-05-16 23:13:09 +0800

    左侧聊天列表集合

--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,8 +1,45 @@
+import { useState } from 'react'
 import './chatlist.css'
 
 const Chatlist = () => {
+
+  const [addMode, setAddMode] = useState(false);
+
   return (
-    <div className='chatlist'>Chatlist</div>
+    <div className='chatlist'>
+      {/** 这是搜索栏 */}
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder='Search' />
+        </div>
+        <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className='add'
+          onClick={() => setAddMode((prev) => !prev)} />
+      </div>
+
+      {/** 这是用户框 */}
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>liu honghong</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>liu honghong</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>liu honghong</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
