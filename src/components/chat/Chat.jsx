6c6cb302d93413f7bfa8d0b6f02795591d389631
Guocork <g1024536444@gmail.com>
2024-05-19 23:12:50 +0800

    ref 新知识

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './chat.css'
 import EmojiPicker from 'emoji-picker-react'
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 const Chat = () => {
 
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth"});
+  },[]);
 
   const handleEmoji = e => {
     setText((prev) => prev + e.emoji);
@@ -72,6 +80,7 @@ const Chat = () => {
             <span>1 min ago</span>
           </div>
         </div>
+        <div ref={endRef}></div>
       </div>
 
       <div className="bottom">
