1502d3e366221e1194bb14427d259a3f05eae1f3
Guocork <g1024536444@gmail.com>
2024-05-20 22:57:36 +0800

    弹出框 ui基本 完善

--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './chatlist.css'
+import AddUser from './addUser/AddUser';
 
 const Chatlist = () => {
 
@@ -67,6 +68,7 @@ const Chatlist = () => {
           <p>Hello</p>
         </div>
       </div>
+      {addMode && <AddUser/>}
     </div>
   )
 }
