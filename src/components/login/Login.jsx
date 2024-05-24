de5138105c69ba9066eedc3f3a6ecdc60a54890b
Guocork <g1024536444@gmail.com>
2024-05-24 21:37:04 +0800

    存储数据

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import './login.css'
 import { toast } from 'react-toastify';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../../lib/firebase';
 import { doc, setDoc } from 'firebase/firestore';
+import upload from '../../lib/upload';
 
 const Login = () => {
 
     const [avatar, setAvatar] = useState({
         file: null,
-        url:""
+        url: ""
     });
+
+    const [loading, setLoading] = useState(false);
 
     const handleAvatar = e => {
         if (e.target.files[0]) {
@@ -24,6 +27,8 @@ const Login = () => {
 
     const handleRegister = async (e) => {
         e.preventDefault(); // 阻止事件的默认行文 阻止提交表单在页面的重新刷新
+        setLoading(true);
+
         const formData = new FormData(e.target);
 
         const { username, email, password } = Object.fromEntries(formData);
@@ -31,27 +36,45 @@ const Login = () => {
         try {
             const res = await createUserWithEmailAndPassword(auth, email, password);
 
-            await setDoc(doc(db, "users",res.user.uid), {
+            const imgUrl = await upload(avatar.file);
+
+            await setDoc(doc(db, "users", res.user.uid), {
                 username,
                 email,
+                avatar: imgUrl,
                 id: res.user.uid,
-                blocked:[],  // 这是被用户屏蔽的其他用户的id列表
+                blocked: [],  // 这是被用户屏蔽的其他用户的id列表
             });
 
-            await setDoc(doc(db, "userschats",res.user.uid), {
-                chats:[]
+            await setDoc(doc(db, "userschats", res.user.uid), {
+                chats: []
             });
 
             toast.success("Account created! You can login now!")
-        } catch(err) {
+        } catch (err) {
             console.log(err);
             toast.error(err.message)
+        } finally {
+            setLoading(false)
         }
-       
+
     };
 
-    const handleLogin = e => {
+    const handleLogin = async e => {
         e.preventDefault();
+        setLoading(true)
+
+        try {
+            const formData = new FormData(e.target);
+            const { email, password } = Object.fromEntries(formData);
+
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.log(err);
+            toast.error(err.message)
+        } finally {
+            setLoading(false)
+        }
     };
 
     return (
@@ -61,7 +84,7 @@ const Login = () => {
                 <form onSubmit={handleLogin}>
                     <input type="text" name="email" placeholder='Email' />
                     <input type="password" placeholder='Password' name='password' />
-                    <button>Sign In</button>
+                    <button disabled={loading}>{loading ? "Loading" : "Sign In"}</button>
                 </form>
             </div>
             <div className="separator"></div>
@@ -72,11 +95,11 @@ const Login = () => {
                         <img src={avatar.url || "./avatar.png"} alt="" />
                         Upload an image
                     </label>
-                    <input type="file" id='file' style={{ display: "none" }} onChange={handleAvatar}/>
+                    <input type="file" id='file' style={{ display: "none" }} onChange={handleAvatar} />
                     <input type="text" name="username" placeholder='Username' />
                     <input type="text" name="email" placeholder='Email' />
                     <input type="password" placeholder='Password' name='password' />
-                    <button>Sign Up</button>
+                    <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
                 </form>
             </div>
         </div>
